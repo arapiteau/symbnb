35adc39ff1ad81af9d2d57a54bf4827093a7f4d7
Dbnk732 <antoine.rapiteau@laposte.net>
2020-06-23 15:58:46 +0200

    Premier commit, installation et page d'accueil

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController {
+
+    /**
+     * Montre la page qui dit bonjour.
+     *
+     * @Route("/hello", name="hello_only")
+     * @Route("/hello/{age}", name="hello_age")
+     * @Route("/hello/{prenom}/{age}", name="hello_name_age")
+     * @return void
+     */
+    public function hello($prenom = 'toi', $age = 0){
+        // return new Response ('Bonjour ' . $prenom . ($age == 0 ? '' : ', vous avez ' . $age . ' ans !'));
+        return $this->render(
+            'hello.html.twig',
+            [
+                'prenom' => 'Magali',
+                'age' => 95    
+            ]
+        );
+    }
+    /**
+     * Undocumented function
+     *
+     * @Route("/", name="homepage")
+     * @return void
+     */
+    public function home(){
+        $prenoms = ["Lior" => 31, "Joseph" => 12, "Anne" => 55];
+
+        return $this->render(
+            'home.html.twig',
+            [ 
+                'title' => 'Bonjour à tous !',
+                'age' => 31,
+                'tableau' => $prenoms
+            ]
+        );
+    }
+
+}
